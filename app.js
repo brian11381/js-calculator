b993a80a7618a7068ebced9a8e99b5ee2e003c02
Brian Ennis <brian11381@gmail.com>
2020-09-13 15:25:17 -0700

    rewrote input handling and added support for floats

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 
-let inputMemory = [];
+let tempNumArray =[];
+let lastInput=[];
+let numArray = [];
+let opArray = [];
+let isLastOperator = false;
 let calcTotal = 0;
-const regEx =/x|\+|\*|-|\//;
+const regExOp =/x|\+|\*|-|\//;
+const regExNum = /[0-9]|\./;
 let display = document.querySelector('#display');
-// const numberMemory = [];
-// const operatorMemory = [];
-
 
 window.onload = function(){
   const btns = document.querySelectorAll('button');
@@ -64,57 +66,172 @@ function operate(num1, num2, oper){
 
 function evaluateInput(){
   let input = this.textContent;
-  const len = inputMemory.length;
-  const lastInput =inputMemory[len-1];
-  if(input.match(regEx)){
-    if(len == 0 || lastInput.match(regEx)){ 
-      console.log('ERROR');
-    }else {
-      inputMemory.push(input);
-    }
-  }else if(input.match(/[0-9]/)){
-    inputMemory.push(input);
-  }else if(input == "="){
-    sortInputMemory();
-  }else if(input == "CE"){
-    ClearInputs();
-    input = '0.0';
-  }else if(input == "<"){
-    inputMemory = inputMemory.slice(0, len-1);
+  if(input.match(regExOp)){
+    inputOperator(input);
   }
-  updateDisplay(input);
-}
-  
-  
-
-
-function sortInputMemory(){
-    let nums = inputMemory.join('').split(regEx).map(Number);
-    let opers = inputMemory.filter(item =>{
-      return item.match(regEx);
-    })
-    while(nums.length > 1){
-    calcTotal = operate(nums[0], nums[1], opers[0]);
-    calcTotal = Math.round(calcTotal * 100)/100;
-    nums.splice(0,2,calcTotal);
-    opers.splice(0,1);
-    }
-    updateDisplay(calcTotal);
-}
-
-function updateDisplay(input){
-  if(input == '='){
-    display.textContent = calcTotal;
+  else if(input.match(regExNum)){
+    inputNumber(input);
   }
   else {
-    display.textContent = input;
+    inputSpecial(input);
   }
 }
 
-function ClearInputs(){
-  calcTotal = 0;
-  inputMemory = [];
+function inputNumber(input){
+  if(input == "."){
+    handleDecimal();
+  }
+  else{
+    tempNumArray.push(input);
+  }
+  isLastOperator = false;
+  updateDisplay(tempNumArray.join(''));
 }
+
+function handleDecimal(){
+  if(tempNumArray.includes(".")){
+    return alert('too many decimals!!')
+  }
+  else {
+    tempNumArray.push(".");
+  }
+}
+
+function inputOperator(input){
+  let len = tempNumArray.length-1;
+  if(tempNumArray[len] =="."){
+    return alert('operator after decimal not allowed!!')
+  }
+  else if(isLastOperator){
+    return alert('only one operator at a time!!!');
+  }
+  else if(!tempNumArray.length){
+    return alert('cant operate without a number!!!');
+  }
+  else{
+    updateDisplay(input);
+    pushValuesToArrays(input);
+  }
+}
+
+function pushValuesToArrays(input){
+  let joined = parseFloat(tempNumArray.join(''));
+  numArray.push(joined);
+  opArray.push(input);
+  tempNumArray = [];
+  isLastOperator = true; 
+}
+
+function updateDisplay(item){
+  display.textContent = item;
+}
+
+function inputSpecial(input){
+  if (input == '='){
+    equalSign();
+  }
+  else if(input == '<'){
+    backSpace();
+  }
+  else if (input == 'CE'){
+    clearValues();
+    display.textContent = '0.0';
+  }
+}
+
+function equalSign(){
+  if(!opArray.length){
+    return alert('nothing to evaluate');
+  }
+  else {
+    pushValuesToArrays();
+    prepareCalc();
+  }
+}
+
+function prepareCalc(){
+  while(numArray.length > 1){
+  calcTotal = operate(numArray[0], numArray[1], opArray[0]);
+  calcTotal = Math.round(calcTotal * 100)/100;
+  numArray.splice(0,2,calcTotal);
+  opArray.splice(0,1);
+  }
+  updateDisplay(calcTotal);
+  clearValues();
+}
+
+function backSpace(){
+  let len = tempNumArray.length;
+  if(len){
+    tempNumArray = tempNumArray.slice(0, len-1);
+    updateDisplay(tempNumArray.join(''));
+  }
+  else {
+    return alert('nothing to delete');
+  }
+}
+
+function clearValues(){
+  calcTotal = 0;
+  numArray = [];
+  opArray = [];
+  tempNumArray = [];
+  isLastOperator = false;
+}
+
+// function evaluateInput(){
+//   let input = this.textContent;
+//   const len = inputMemory.length;
+//   const lastInput =inputMemory[len-1];
+//   if(input.match(regEx)){
+//     if(len == 0 || lastInput.match(regEx)){ 
+//       console.log('ERROR');
+//     }else {
+//       inputMemory.push(input);
+//     }
+//   }else if(input.match(/[0-9]/)){
+//     inputMemory.push(input);
+//   }else if(input == "="){
+//     sortInputMemory();
+//   }else if(input == "CE"){
+//     ClearInputs();
+//     input = '0.0';
+//   }else if(input == "<"){
+//     inputMemory = inputMemory.slice(0, len-1);
+//   }
+//   updateDisplay(input);
+// }
+  
+  
+
+
+// function sortInputMemory(){
+//     let nums = inputMemory.join('').split(regEx).map(Number);
+//     let opers = inputMemory.filter(item =>{
+//       return item.match(regEx);
+//     })
+    // while(numArray.length > 1){
+    // calcTotal = operate(numArray[0], numArray[1], opArray[0]);
+    // calcTotal = Math.round(calcTotal * 100)/100;
+    // numArray.splice(0,2,calcTotal);
+    // opArray.splice(0,1);
+    // }
+    // updateDisplay(calcTotal);
+// }
+
+// function updateDisplay(input){
+//   if(input == '='){
+//     display.textContent = calcTotal;
+//   }
+//   else {
+//     display.textContent = input;
+//   }
+// }
+
+// function ClearInputs(){
+//   calcTotal = 0;
+//   inputMemory = [];
+// }
 
 
 
